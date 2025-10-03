58eb4c3c5dcab3ebf6c2e615115c4f175ae36d4e
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2025-10-03 15:55:41 -0400

    assn: finish "Postgres Database on Render"

--- project/app.py
+++ project/app.py
@@ -1,6 +1,7 @@
 import sqlite3
 from pathlib import Path
 from functools import wraps
+import os
 
 from flask import Flask, g, render_template, request, session, \
                   flash, redirect, url_for, abort, jsonify
@@ -14,7 +15,14 @@ DATABASE = "flaskr.db"
 USERNAME = "admin"
 PASSWORD = "admin"
 SECRET_KEY = "change_me"
-SQLALCHEMY_DATABASE_URI = f'sqlite:///{Path(basedir).joinpath(DATABASE)}'
+
+# Support for Postgres on Render
+url = os.getenv('DATABASE_URL', f'sqlite:///{Path(basedir).joinpath(DATABASE)}')
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+SQLALCHEMY_DATABASE_URI = url
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
