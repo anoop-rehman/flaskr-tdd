e40a6e933d9d48e7d44e24953d34f38b2ef320c6
Anoop Rehman <93463679+anoop-rehman@users.noreply.github.com>
2025-10-03 15:34:59 -0400

    assn: finish "Search Page"

--- tests/app_test.py
+++ tests/app_test.py
@@ -81,3 +81,10 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+
+def test_search(client):
+    """Ensure search page loads"""
+    rv = client.get('/search/')
+    assert rv.status_code == 200
+    assert b"Search" in rv.data
